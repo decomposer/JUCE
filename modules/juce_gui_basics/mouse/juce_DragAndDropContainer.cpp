10d2bda5f15e7e5d4ccef65d171291157f4c381f
Lucijan Busch <lucijan@directededge.com>
2022-09-28 20:43:20 +0200

    Show drag and drop image when run as AUv3

--- modules/juce_gui_basics/mouse/juce_DragAndDropContainer.cpp
+++ modules/juce_gui_basics/mouse/juce_DragAndDropContainer.cpp
@@ -462,12 +462,25 @@ void DragAndDropContainer::startDragging (const var& sourceDescription,
 
     if (allowDraggingToExternalWindows)
     {
-        if (! Desktop::canUseSemiTransparentWindows())
-            dragImageComponent->setOpaque (true);
+        if (SystemStats::isRunningInAppExtensionSandbox())
+        {
+            if (auto* thisComp = dynamic_cast<Component*> (this))
+            {
+                auto topLevel = thisComp->getTopLevelComponent();
+                if (topLevel)
+                    topLevel->addChildComponent (dragImageComponent);
+            }
 
-        dragImageComponent->addToDesktop (ComponentPeer::windowIgnoresMouseClicks
-                                          | ComponentPeer::windowIsTemporary
-                                          | ComponentPeer::windowIgnoresKeyPresses);
+        }
+        else
+        {
+            if (! Desktop::canUseSemiTransparentWindows())
+                dragImageComponent->setOpaque (true);
+
+            dragImageComponent->addToDesktop (ComponentPeer::windowIgnoresMouseClicks
+                                              | ComponentPeer::windowIsTemporary
+                                              | ComponentPeer::windowIgnoresKeyPresses);
+        }
     }
     else
     {
