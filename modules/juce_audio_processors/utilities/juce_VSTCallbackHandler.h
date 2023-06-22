dbeab43b6bd618825d7d385d479643f09439cf58
Scott Wheeler <scott@directededge.com>
2023-06-22 11:37:51 +0200

    Silence warnings

--- modules/juce_audio_processors/utilities/juce_VSTCallbackHandler.h
+++ modules/juce_audio_processors/utilities/juce_VSTCallbackHandler.h
@@ -78,6 +78,7 @@ struct VSTCallbackHandler
 
     virtual bool handleVstNoteName(int32 channel, int32 note, String &name)
     {
+        ignoreUnused (channel, note, name);
         return false;
     }
 };
